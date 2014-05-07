2c89f69379ca81762c9352b18f1a0924fce2ad25
ehime <dodomeki@gmail.com>
2014-05-06 19:36:51 -0700

    Added parser/cleaner

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-<?php
-echo 'working';
+<html>
+    <body>
+
+    <form enctype='multipart/form-data'
+          action='parser.php'
+          method='post'
+    >
+
+        <input type='file' name='file' id='file'><br />
+        <input type='submit' name='clean' value='Clean'>
+
+    </form>
+
+    </body>
+</html>
